2ad73c1578448d316203d7d658532cf278f092a8
xiangbenguo@codkid.com <xiangbenguo@codkid.com>
2019-03-12 19:02:09 +0800

    实现产品图片api对接

--- src/pages/picture.vue
+++ src/pages/picture.vue
@@ -10,7 +10,7 @@
                       </el-table-column>
                       <el-table-column  prop="singleImgSrc" label="产品单个图片" width="300">
                           <template slot-scope="scope">
-                              <img :src="scope.row.singleImgSrc" alt="" class="singleImg">
+                              <img :src="url(scope.row.url)" alt="" class="singleImg">
                           </template>
                       </el-table-column>
                       <el-table-column  label="删除" width="100">
@@ -22,8 +22,10 @@
                   <div class="addImg">
                     <el-upload
                       class="upload-demo"
-                      action="https://jsonplaceholder.typicode.com/posts/"
+                      action="http://localhost:8080/file/upload/"
                       :on-preview="handlePreview"
+                      :on-success="singleHandleSuccess"
+                      :before-upload="singlePictureNum"
                       :file-list="fileList">
                       <el-button size="small" type="primary">点击上传</el-button>
                       <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
@@ -36,7 +38,7 @@
                       </el-table-column>
                       <el-table-column  prop="detailImgSrc" label="产品详情图片" width="300">
                           <template slot-scope="scope">
-                              <img :src="scope.row.detailImgSrc" alt="" class="singleImg">
+                              <img :src="url(scope.row.url)" alt="" class="singleImg">
                           </template>
                       </el-table-column>
                       <el-table-column  label="删除" width="100">
@@ -48,8 +50,10 @@
                   <div class="addImg">
                     <el-upload
                       class="upload-demo"
-                      action="https://jsonplaceholder.typicode.com/posts/"
+                      action="http://localhost:8080/file/upload/"
                       :on-preview="handlePreview"
+                      :on-success="detailHandleSuccess"
+                      :before-upload="detailPictureNum"
                       :file-list="fileList">
                       <el-button size="small" type="primary">点击上传</el-button>
                       <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
@@ -66,42 +70,8 @@ import Header from '../components/web-header'
 export default {
   data () {
     return {
-      singleImgList: [
-        {
-          id: '1',
-          singleImgSrc: 'http://how2j.cn/tmall/img/productSingle/8620.jpg'
-        }, {
-          id: '2',
-          singleImgSrc: 'http://how2j.cn/tmall/img/productSingle/8619.jpg'
-        }, {
-          id: '3',
-          singleImgSrc: 'http://how2j.cn/tmall/img/productSingle/8618.jpg'
-        }, {
-          id: '4',
-          singleImgSrc: 'http://how2j.cn/tmall/img/productSingle/8617.jpg'
-        }, {
-          id: '5',
-          singleImgSrc: 'http://how2j.cn/tmall/img/productSingle/8616.jpg'
-        }
-      ],
-      detailImgList: [
-        {
-          id: '1',
-          detailImgSrc: 'http://how2j.cn/tmall/img/productSingle/8620.jpg'
-        }, {
-          id: '2',
-          detailImgSrc: 'http://how2j.cn/tmall/img/productSingle/8619.jpg'
-        }, {
-          id: '3',
-          detailImgSrc: 'http://how2j.cn/tmall/img/productSingle/8618.jpg'
-        }, {
-          id: '4',
-          detailImgSrc: 'http://how2j.cn/tmall/img/productSingle/8617.jpg'
-        }, {
-          id: '5',
-          detailImgSrc: 'http://how2j.cn/tmall/img/productSingle/8616.jpg'
-        }
-      ],
+      singleImgList: [],
+      detailImgList: [],
       formLabelWidth: '80px',
       form: {
         propertyName: ''
@@ -120,10 +90,16 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        this.$message({
-          type: 'success',
-          message: '成功删除!'
-        });
+        this.$axios.post(`${this.$restUrl}/productImg/delete`, {'id': value.id}).then((res) => {
+          console.log(res)
+            this.$message({
+            type: 'success',
+            message: '成功删除!'
+          })
+          this.getList()
+        }).catch((err) => {
+          console.log(err)
+        })
       }).catch(() => {
         this.$message({
           type: 'info',
@@ -134,9 +110,73 @@ export default {
     handlePreview(file) {
       console.log(file);
     },
+    singleHandleSuccess(res) {
+      console.log(res)
+      const data = {
+        pid: this.$route.query.pid,
+        url: res.data,
+        type: 1
+      }
+      this.$axios.post(`${this.$restUrl}/productImg/add`, data).then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    detailHandleSuccess(res) {
+      console.log(res)
+      const data = {
+        pid: this.$route.query.pid,
+        url: res.data,
+        type: 2
+      }
+      this.$axios.post(`${this.$restUrl}/productImg/add`, data).then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    getList () {
+      this.singleImgList = []
+      this.detailImgList = []
+      this.$axios.get(`${this.$restUrl}/productImg/getPidList?pid=${this.$route.query.pid}`).then((res) => {
+        console.log(res)
+        for (var i = 0; i < res.data.data.length; i++) {
+          if (res.data.data[i].type === 1) {
+            this.singleImgList.push(res.data.data[i])
+          } else {
+            this.detailImgList.push(res.data.data[i])
+          }
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    url (src) {
+      return require(`E://upload/${src}`)
+    },
+    singlePictureNum () {
+      if (this.singleImgList.length > 4) {
+        this.$alert('最多只能上传5张图片','提示', {
+          confirmButtonText: '确定',
+          type: 'warning'
+        })
+      return false
+      }
+    },
+    detailPictureNum () {
+      if (this.detailImgList.length > 4) {
+        this.$alert('最多只能上传5张图片','提示', {
+          confirmButtonText: '确定',
+          type: 'warning'
+        })
+      return false
+      }
+    }
   },
   created () {
     console.log(this.$route.query.pid)
+    this.getList()
   }
 }
 </script>
