3936771430c9f3ef52d924f0ce467e401d02ea23
xiangbenguo@codkid.com <xiangbenguo@codkid.com>
2019-03-19 17:38:10 +0800

    订单api对接

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -7,14 +7,14 @@
          <el-table  :data="infoList"  border  style="width: 100%">
             <el-table-column type="expand">
                 <template slot-scope="props">
-                    <a :href="`localhost:8080/#/productInfo?pid=${props.row.pid}`" target="_blank" class="productInfoSrc">{{props.row.productName}}</a>
+                    <a :href="`localhost:8082/#/productInfo?pid=${props.row.pid}`" target="_blank" class="productInfoSrc">{{props.row.productName}}</a>
                 </template>
             </el-table-column>
             <el-table-column  prop="id"  label="ID"  width="60">
             </el-table-column>
             <el-table-column  prop="status" label="状态" width="100">
             </el-table-column>
-            <el-table-column  prop="amount" label="金额" width="100">
+            <el-table-column  prop="productPrice" label="金额" width="100">
             </el-table-column>
             <el-table-column  prop="number" label="数量" width="60">
             </el-table-column>
@@ -45,61 +45,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      infoList: [
-        {
-          id: '1',
-          productName: '阿迪达斯裤子男秋冬季休闲裤加绒卫裤长裤小脚男裤收口束脚运动裤',
-          pid: '10',
-          status: '待支付',
-          amount: '￥2000',
-          number: '2',
-          BuyerName: '向本国',
-          creationTime: '2019-2-25',
-          paymentTime: '',
-          deliveryTime: '',
-          receiveTime: '',
-          operation: false
-        }, {
-          id: '2',
-          productName: '阿迪达斯裤子男秋冬季休闲裤加绒卫裤长裤小脚男裤收口束脚运动裤',
-          pid: '10',
-          status: '待发货',
-          amount: '￥2000',
-          number: '2',
-          BuyerName: '向本国',
-          creationTime: '2019-2-25',
-          paymentTime: '2019-2-25',
-          deliveryTime: '',
-          receiveTime: '',
-          operation: true
-        }, {
-          id: '3',
-          productName: '阿迪达斯裤子男秋冬季休闲裤加绒卫裤长裤小脚男裤收口束脚运动裤',
-          pid: '10',
-          status: '待收货',
-          amount: '￥2000',
-          number: '2',
-          BuyerName: '向本国',
-          creationTime: '2019-2-25',
-          paymentTime: '2019-2-25',
-          deliveryTime: '2019-2-25',
-          receiveTime: '',
-          operation: false
-        }, {
-          id: '4',
-          productName: '阿迪达斯裤子男秋冬季休闲裤加绒卫裤长裤小脚男裤收口束脚运动裤',
-          pid: '10',
-          status: '已完成',
-          amount: '￥2000',
-          number: '2',
-          BuyerName: '向本国',
-          creationTime: '2019-2-25',
-          paymentTime: '2019-2-25',
-          deliveryTime: '2019-2-25',
-          receiveTime: '2019-2-25',
-          operation: false
-        }
-      ]
+      infoList: []
     }
   },
   components: {
@@ -107,8 +53,67 @@ export default {
   },
   methods: {
     handleclick (value) {
-      console.log(value)
+      this.theDelivery(value.id)
+    },
+    theDelivery (oid) {
+      var data = {
+        'id': oid,
+        'status': 3
+      }
+      this.$axios.post(`http://localhost:8080/order/theDelivery`, data).then((res) => {
+        console.log(res)
+        if (res.data.code === 200) {
+          this.$alert('已发货', '提示', {
+            confirmButtonText: '确定'
+          })
+          this.getOrderList()
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    getOrderList () {
+      this.$axios.get('http://localhost:8080/order/list').then((res) => {
+        console.log(res)
+        this.infoList = []
+        for (var i = 0; i < res.data.data.length; i++) {
+          var obj = {
+            pid: res.data.data[i].pid,
+            id: res.data.data[i].id,
+            productPrice: '',
+            productName: '',
+            status: res.data.data[i].status,
+            amount: '',
+            number: res.data.data[i].amount,
+            BuyerName: res.data.data[i].receiver,
+            creationTime: res.data.data[i].createtime.replace('.000+0000', ''),
+            paymentTime: res.data.data[i].payTime === null ? '' : res.data.data[i].payTime.replace('.000+0000', ''),
+            deliveryTime: res.data.data[i].deliveryTime === null ? '' : res.data.data[i].deliveryTime.replace('.000+0000', ''),
+            receiveTime: res.data.data[i].confirmTime === null ? '' : res.data.data[i].confirmTime.replace('.000+0000', ''),
+            operation: res.data.data[i].status === 2
+          }
+          this.infoList.push(obj)
+          this.getProductInfo(res.data.data[i].pid, i)
+        }
+        console.log(this.infoList)
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    getProductInfo (pid, index) {
+      this.$axios.get(`http://localhost:8080/product/get?id=${pid}`).then((res) => {
+        console.log(res)
+        if (res.data.code === 200) {
+          this.infoList[index].productName = res.data.data.name
+          this.infoList[index].productPrice = res.data.data.price
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
     }
+  },
+  created () {
+    this.getOrderList()
   }
 }
 </script>
